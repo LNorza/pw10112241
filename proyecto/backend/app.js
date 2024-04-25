12d3459f44948cd387d1ae0746f13f0f78274632
LNorza <luiscnorza@gmail.com>
2024-04-25 11:49:33 -0600

    Connection DB

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -11,6 +11,35 @@ app.get('/', (request, response) => {
     response.send("Ruta de iniciosss");
 })
 
+//Conexion a MySQL
+let connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Invermex2018!',
+    database: 'pwdata',
+    port: 3306,
+});
+connection.connect((error) => {
+    if (error) {
+        throw error;
+    }
+    console.log('Conexion con base de datos MySQL establecida');
+})
+
+//Seleccionar todos los Clientes:
+app.get('/api/clientes', (request, response) => {
+    const queryData = "SELECT * FROM clientes";
+    connection.query(queryData, (error, filas) => {
+        if (error) {
+            throw error;
+        } else {
+            response.send(filas);
+        }
+    });
+})
+
+
+
 
 
 //Encender Servidor
