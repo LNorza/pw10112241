137a05ebda988666cac54cd9706e41483e94a580
LNorza <luiscnorza@gmail.com>
2024-05-08 11:59:33 -0600

    add bootstrap navbar

---
+++ proyecto/frontend/tienda/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import MenuView from './views/MenuView.vue';
+</script>
+
+<template>
+  <header>
+    <MenuView />
+  </header>
+
+  <RouterView />
+</template>
