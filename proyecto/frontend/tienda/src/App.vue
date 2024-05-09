269fee925dd8faa53334c75ec2d42d584f99f7d3
LNorza <luiscnorza@gmail.com>
2024-05-09 11:58:08 -0600

    Create CustomerView

--- proyecto/frontend/tienda/src/App.vue
+++ proyecto/frontend/tienda/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 import MenuView from './views/MenuView.vue';
 </script>
 
 <template>
-  <header>
-    <MenuView />
-  </header>
+    <header>
+        <MenuView />
+    </header>
 
-  <RouterView />
+    <RouterView />
 </template>
---
+++ proyecto/frontend/tienda/src/views/ClientsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Customers
+                    <RouterLink to="/clients/new" class="btn btn-primary float-end">
+                        Add
+                    </RouterLink>
+                </h4>
+            </div>
+
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</template>
